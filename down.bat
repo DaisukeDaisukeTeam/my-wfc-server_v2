767b370a18b39eca5e61a78ec765b86ccba53bbd
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2025-10-24 07:38:20 +0900

    fix

--- down.bat
+++ down.bat
@@ -1,3 +1,2 @@
-cd D:\software\pkmn-wfc-server
 docker-compose down -v --rmi all --volumes --remove-orphans
 pause
--- up.bat
+++ up.bat
@@ -1,4 +1,3 @@
-cd D:\software\pkmn-wfc-server
 docker-compose down -v --rmi all --volumes --remove-orphans
 docker volume prune -f
 docker compose up -d --build
